4b942c316429a8e52d7f8ef369cd52743e1425b6
Taciano Morais Silva <tacianosilva@gmail.com>
2017-10-05 15:53:23 -0300

    Adicionando classe da aplicação Spring Boot.

---
+++ src/main/java/br/ufrn/dct/apf/APFApplication.java
@@ -0,0 +1,12 @@
+package br.ufrn.dct.apf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APFApplication.class, args);
+	}
+}
