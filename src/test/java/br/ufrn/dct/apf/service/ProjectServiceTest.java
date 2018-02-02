94c83bc1994d63ea188c33b9fc3764622c404b77
Taciano Morais Silva <tacianosilva@gmail.com>
2018-02-01 21:24:23 -0300

    Arquivos com a suite de testes e alterações.

--- src/test/java/br/ufrn/dct/apf/service/ProjectServiceTest.java
+++ src/test/java/br/ufrn/dct/apf/service/ProjectServiceTest.java
@@ -3,14 +3,21 @@ package br.ufrn.dct.apf.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import br.ufrn.dct.apf.model.Project;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration("/spring-test-beans.xml")
+@DataJpaTest
+// @EnableJpaRepositories
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectServiceTest extends AbstractTestNGSpringContextTests {
 
     private SoftAssert softAssert;
@@ -18,24 +25,31 @@ public class ProjectServiceTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private ProjectService service;
 
-    //@Test
+    @BeforeMethod
+    public void startTest() {
+        softAssert = new SoftAssert();
+    }
+
+    // @Test
     public void delete() {
         throw new RuntimeException("Test not implemented");
     }
 
-    //@Test
+    @Test
     public void findAll() {
         List<Project> projetos = service.findAll();
         softAssert.assertNotNull(projetos, "Teste 01");
         softAssert.assertEquals(projetos.size(), 2, "Teste 02");
+        
+        softAssert.assertAll();
     }
 
-    //@Test
+    // @Test
     public void findOne() {
         throw new RuntimeException("Test not implemented");
     }
 
-    //@Test
+    // @Test
     public void save() {
         throw new RuntimeException("Test not implemented");
     }
