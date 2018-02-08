9cf164d461772c750d911ad1e548bea26ca0083c
Taciano Morais Silva <tacianosilva@gmail.com>
2018-02-07 22:04:00 -0300

    Teste de Unidade para ProjectService completo (issue #9).

--- src/test/java/br/ufrn/dct/apf/service/ProjectServiceTest.java
+++ src/test/java/br/ufrn/dct/apf/service/ProjectServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -23,27 +24,31 @@ public class ProjectServiceTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private ProjectService service;
+    
+    private Project p1;
 
     @BeforeMethod
     public void startTest() {
         softAssert = new SoftAssert();
-    }
-
-    // @Test
-    public void delete() {
-        throw new RuntimeException("Test not implemented");
-    }
-
-    @Test
-    public void findAll() {
-        Project p1 = new Project();
+        
+        p1 = new Project();
         
         p1.setName("APF Project");
         p1.setDescription("Analisador de Pontos por Função");
         p1.setCreated(GregorianCalendar.getInstance().getTime());
         
         service.save(p1);
-        
+    }
+    
+    @AfterMethod
+    public void endTest() {
+        softAssert = null;
+        service.delete(p1.getId());
+        p1 = null;
+    }
+
+    @Test
+    public void findAll() {
         List<Project> projetos = service.findAll();
         
         softAssert.assertNotNull(projetos, "T01 - NotNull:");
@@ -54,15 +59,6 @@ public class ProjectServiceTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void findOne() {
-        Project p1 = new Project();
-        
-        p1.setName("APF Project");
-        p1.setDescription("Analisador de Pontos por Função");
-        p1.setCreated(GregorianCalendar.getInstance().getTime());
-        p1.setActive(1);
-        
-        service.save(p1);
-        
         Long id = p1.getId();
         
         Project found = service.findOne(id);
@@ -79,8 +75,94 @@ public class ProjectServiceTest extends AbstractTestNGSpringContextTests {
         softAssert.assertAll();
     }
 
-    // @Test
+    @Test
     public void save() {
-        throw new RuntimeException("Test not implemented");
+        Project p2 = new Project();
+        
+        p2.setName("Test Project");
+        p2.setDescription("TestNG Project");
+        p2.setCreated(GregorianCalendar.getInstance().getTime());
+        p2.setActive(1);
+        
+        service.save(p2);
+        
+        Long id = p2.getId();
+        
+        Project found = service.findOne(id);
+        List<Project> projetos = service.findAll();
+        
+        softAssert.assertNotNull(id, "T01 - NotNull:");
+        softAssert.assertNotNull(found, "T02 - NotNull:");
+        
+        softAssert.assertNotNull(projetos, "T03 - NotNull:");
+        softAssert.assertEquals(projetos.size(), 2, "T04 - Equals:");
+        
+        softAssert.assertEquals(found.getName(), p2.getName(), "T05 - Equals:");
+        softAssert.assertEquals(found.getDescription(), p2.getDescription(), "T06 - Equals:");
+        
+        softAssert.assertNotNull(found.getCreated(), "T07 - NotNull:");
+        softAssert.assertNotNull(found.getActive(), "T08 - NotNull:");
+        softAssert.assertEquals(found.getActive(), p2.getActive(), "T08 - Equals:");
+        
+        service.delete(id);
+        
+        softAssert.assertAll();
+    }
+    
+    @Test
+    public void update() {
+        Long id = p1.getId();
+        
+        Project update = service.findOne(id);
+
+        update.setName("Novo Projeto");
+        update.setDescription("Novo projeto para teste de update");
+        
+        service.save(update);
+        
+        Project found = service.findOne(id);
+        
+        softAssert.assertNotNull(found, "T01 - NotNull:");
+        
+        softAssert.assertEquals(found.getName(), "Novo Projeto", "T02 - Equals:");
+        softAssert.assertEquals(found.getDescription(), "Novo projeto para teste de update", "T03 - Equals:");
+        softAssert.assertEquals(found.getActive(), 0, "T04 - Equals:");
+
+        softAssert.assertAll();
+    }
+    
+    @Test
+    public void delete() {
+        Project p2 = new Project();
+        
+        p2.setName("Test Project");
+        p2.setDescription("TestNG Project");
+        p2.setCreated(GregorianCalendar.getInstance().getTime());
+        p2.setActive(1);
+        
+        service.save(p2);
+        
+        Long id = p2.getId();
+        
+        Project found = service.findOne(id);
+        List<Project> projetos = service.findAll();
+        
+        softAssert.assertNotNull(id, "T01 - NotNull:");
+        softAssert.assertNotNull(found, "T02 - NotNull:");
+        
+        softAssert.assertNotNull(projetos, "T03 - NotNull:");
+        softAssert.assertEquals(projetos.size(), 2, "T04 - Equals:");
+        
+        service.delete(id);
+        
+        found = service.findOne(id);
+        projetos = service.findAll();
+        
+        softAssert.assertNull(found, "T05 - NotNull:");
+        
+        softAssert.assertNotNull(projetos, "T06 - NotNull:");
+        softAssert.assertEquals(projetos.size(), 1, "T07 - Equals:");
+        
+        softAssert.assertAll();
     }
 }
