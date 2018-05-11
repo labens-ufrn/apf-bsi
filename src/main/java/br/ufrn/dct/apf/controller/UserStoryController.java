54cd4b37a3c0a3bf38ff103f85f336a2b0d6397a
Taciano Morais Silva <tacianosilva@gmail.com>
2018-05-11 18:50:52 -0300

    Remoção de code smells.

--- src/main/java/br/ufrn/dct/apf/controller/UserStoryController.java
+++ src/main/java/br/ufrn/dct/apf/controller/UserStoryController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufrn.dct.apf.model.Project;
-import br.ufrn.dct.apf.model.User;
 import br.ufrn.dct.apf.model.UserStory;
 import br.ufrn.dct.apf.service.ProjectService;
 import br.ufrn.dct.apf.service.UserStoryService;
@@ -26,61 +24,56 @@ import br.ufrn.dct.apf.service.UserStoryService;
 @Controller
 public class UserStoryController extends AbstractController {
 
-        @Autowired
-        private UserStoryService service;
-        
-        @Autowired
-        private ProjectService projectService;
+    @Autowired
+    private UserStoryService service;
 
-        @GetMapping("/us/add")
-        public ModelAndView add(UserStory us) {
+    @Autowired
+    private ProjectService projectService;
 
-            ModelAndView mv = new ModelAndView("us/add");
-            mv.addObject("userstory", us);
+    @GetMapping("/us/add")
+    public ModelAndView add(UserStory us) {
 
-            return mv;
+        ModelAndView mv = new ModelAndView("us/add");
+        mv.addObject("userstory", us);
+
+        return mv;
+    }
+
+    @PostMapping("/us/save")
+    public ModelAndView save(@Valid UserStory us, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return add(us);
         }
 
-        @PostMapping("/us/save")
-        public ModelAndView save(@Valid UserStory us, BindingResult result) {
+        service.save(us);
 
-            if(result.hasErrors()) {
-                return add(us);
-            }
-            
-            User owner = getCurrentUser();
-            
-            service.save(us);
+        return add(null);
+    }
 
-            return add(null);
-        }
-        
-        /**
-         * the rest controller which is requested by the autocomplete input field
-         * instead of the countries here could also be made a DB request
-         */
-        @RequestMapping(value = "/suggestion", method = RequestMethod.GET, produces = "application/json")
-        @ResponseBody
-        public ProjectSuggestionWrapper autocompleteSuggestions(@RequestParam("searchstr") String searchstr) {
-          System.out.println("searchstr: " + searchstr);
+    /**
+     * the rest controller which is requested by the autocomplete input field
+     * instead of the countries here could also be made a DB request
+     */
+    @RequestMapping(value = "/suggestion", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ProjectSuggestionWrapper autocompleteSuggestions(@RequestParam("searchstr") String searchstr) {
+        System.out.println("searchstr: " + searchstr);
 
+        List<Project> suggestions = new ArrayList<>();
 
-          List<Project> suggestions = new ArrayList<>();
-          
-          List<Project> proj = projectService.findByUserId(null);
-          
-          //List<Project> proj = projectService.findByName(searchstr);
+        List<Project> proj = projectService.findByUserId(null);
 
-          suggestions.addAll(proj);
+        // List<Project> proj = projectService.findByName(searchstr);
 
-          // truncate the list to the first n, max 20 elements
-          int n = suggestions.size() > 20 ? 20 : suggestions.size();
-          List<Project> sulb = new ArrayList<>(suggestions.subList(0, n));
+        suggestions.addAll(proj);
 
-          ProjectSuggestionWrapper sw = new ProjectSuggestionWrapper();
-          sw.setSuggestions(sulb);
-          return sw;
-        }
+        // truncate the list to the first n, max 20 elements
+        int n = suggestions.size() > 20 ? 20 : suggestions.size();
+        List<Project> sulb = new ArrayList<>(suggestions.subList(0, n));
 
-
+        ProjectSuggestionWrapper sw = new ProjectSuggestionWrapper();
+        sw.setSuggestions(sulb);
+        return sw;
+    }
 }
