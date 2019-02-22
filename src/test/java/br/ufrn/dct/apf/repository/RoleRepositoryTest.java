e41916d77b7130117388df63688218fa26ccfde5
Taciano Morais Silva <tacianosilva@gmail.com>
2019-02-22 16:47:06 -0300

    Adição da classe de testes para RoleRepository.

---
+++ src/test/java/br/ufrn/dct/apf/repository/RoleRepositoryTest.java
@@ -0,0 +1,61 @@
+package br.ufrn.dct.apf.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import br.ufrn.dct.apf.model.Role;
+
+@ContextConfiguration("/spring-test-beans.xml")
+@DataJpaTest
+public class RoleRepositoryTest extends AbstractTestNGSpringContextTests {
+
+    private SoftAssert softAssert;
+
+    @Autowired
+    private RoleRepository repository;
+
+    @BeforeMethod
+    public void startTest() {
+        softAssert = new SoftAssert();
+    }
+
+    @AfterMethod
+    public void endTest() {
+        softAssert = null;
+    }
+
+    @Test
+    public void findByName() {
+        String adminRole = Role.ADMIN_ROLE;
+
+        List<Role> regras = repository.findAll();
+        
+        for (Role role : regras) {
+            System.out.println("Role Name: " + role.getRoleName());
+        }
+        
+        softAssert.assertEquals(regras.size(), 4, "T00 - Equals:");
+        
+        Role regra = repository.findByRoleName(adminRole);
+
+        softAssert.assertNotNull(regra, "T01 - NotNull:");
+        softAssert.assertEquals(regra.getRoleName(), "ADMIN", "T02 - Equals:");
+        
+        String managerRole = Role.PROJECT_MANAGER_ROLE;
+
+        Role manager = repository.findByRoleName(managerRole);
+
+        softAssert.assertNotNull(manager, "T03 - NotNull:");
+        softAssert.assertEquals(manager.getRoleName(), "PROJECT MANAGER", "T04 - Equals:");
+
+        softAssert.assertAll();
+    }
+}
