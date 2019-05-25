ea0e8a987451ca0b83e6284184d3d77cb2b2a630
João Eudes Lima <joaoeudes7@gmail.com>
2019-05-25 20:32:45 -0300

    Update configs

--- src/main/java/br/ufrn/dct/apf/configuration/WebMvcConfig.java
+++ src/main/java/br/ufrn/dct/apf/configuration/WebMvcConfig.java
@@ -1,16 +1,16 @@
 package br.ufrn.dct.apf.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// >> https://www.baeldung.com/web-mvc-configurer-adapter-deprecated
+// public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+public class WebMvcConfig implements WebMvcConfigurer {
+     @Autowired
+     public BCryptPasswordEncoder passwordEncoder;
 
 }
