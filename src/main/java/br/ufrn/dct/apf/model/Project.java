3a26c557e2a59f30246aeeb51ae0c7548b387e10
Taciano Morais Silva <tacianosilva@gmail.com>
2019-08-16 22:52:00 -0300

    Ajustes no mapeamento do atributo isPrivate de Projeto.

--- src/main/java/br/ufrn/dct/apf/model/Project.java
+++ src/main/java/br/ufrn/dct/apf/model/Project.java
@@ -46,7 +46,7 @@ public class Project implements Serializable {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createdOn;
 
-    @Column(name = "isPrivate", columnDefinition = "boolean default false")
+    @Column(name = "isPrivate", columnDefinition = "BIT", length = 1)
     @NotNull
     private Boolean isPrivate;
 
