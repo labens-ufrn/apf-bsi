367672502d0e451e614fb832a61bb413d2e99bdd
Taciano Morais Silva <tacianosilva@gmail.com>
2020-06-28 22:29:47 -0300

    Add contagem estimativa e tabela de pontos de função.

---
+++ src/main/java/br/ufrn/dct/apf/count/FunctionalSize.java
@@ -0,0 +1,28 @@
+package br.ufrn.dct.apf.count;
+
+public class FunctionalSize {
+    
+    protected static final String FUNCTIONAL_COMPLEXITY_LOW = "Low";
+    protected static final String FUNCTIONAL_COMPLEXITY_AVERAGE = "Average";
+    protected static final String FUNCTIONAL_COMPLEXITY_HIGH = "High";
+    
+    protected static final int FUNCTION_POINT_ILF_LOW = 7;
+    protected static final int FUNCTION_POINT_ILF_AVERAGE = 10;
+    protected static final int FUNCTION_POINT_ILF_HIGH = 15;
+    
+    protected static final int FUNCTION_POINT_EIF_LOW = 5;
+    protected static final int FUNCTION_POINT_EIF_AVERAGE = 7;
+    protected static final int FUNCTION_POINT_EIF_HIGH = 10;
+    
+    protected static final int FUNCTION_POINT_EI_LOW = 3;
+    protected static final int FUNCTION_POINT_EI_AVERAGE = 4;
+    protected static final int FUNCTION_POINT_EI_HIGH = 6;
+    
+    protected static final int FUNCTION_POINT_EO_LOW = 4;
+    protected static final int FUNCTION_POINT_EO_AVERAGE = 5;
+    protected static final int FUNCTION_POINT_EO_HIGH = 6;
+    
+    protected static final int FUNCTION_POINT_EQ_LOW = 3;
+    protected static final int FUNCTION_POINT_EQ_AVERAGE = 4;
+    protected static final int FUNCTION_POINT_EQ_HIGH = 6;
+}
