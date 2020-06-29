ac7ba8f0b4f2a2803e3242a2aae6fbe4241c4965
Taciano Morais Silva <tacianosilva@gmail.com>
2020-06-28 22:33:20 -0300

    Add modificador abstract.

--- src/test/java/br/ufrn/dct/apf/controller/AbstractControllerTest.java
+++ src/test/java/br/ufrn/dct/apf/controller/AbstractControllerTest.java
@@ -2,6 +2,6 @@ package br.ufrn.dct.apf.controller;
 
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
-public class AbstractControllerTest extends AbstractTestNGSpringContextTests {
+public abstract class AbstractControllerTest extends AbstractTestNGSpringContextTests {
 
 }
